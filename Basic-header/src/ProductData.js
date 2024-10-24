a522006c098f9d8a2ff92e9e062bd06b9f1cdf1c
IsraelRex <46507005+Israelrex9@users.noreply.github.com>
2024-10-24 22:08:41 +0100

    removed data from app.jsx

---
+++ Basic-header/src/ProductData.js
@@ -0,0 +1,28 @@
+
+    const ProductData = [
+        {
+            title : "Gucci Bag",
+            description : "Top leather bag, aliggator skin",
+            price : 13
+        },
+        {
+            title : "Gucci Bag",
+            description : "Top leather bag, aliggator skin",
+            price : 13
+        },
+        {
+            title : "Gucci Bag",
+            description : "Top leather bag, aliggator skin",
+            price : 13
+        },
+        {
+            title : "Gucci Bag",
+            description : "Top leather bag, aliggator skin",
+            price : 13
+        },
+
+    ]
+
+
+
+export default ProductData
