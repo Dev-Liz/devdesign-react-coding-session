186e0f28dfad388a07263b9c076b45c9f8d9c363
Eniola Sola-Kehinde <114470813+EverPleroma@users.noreply.github.com>
2024-10-31 23:53:00 +0100

    code updated

--- modal-component/src/DefaultButton.jsx
+++ modal-component/src/DefaultButton.jsx
@@ -4,17 +4,6 @@ import Button from "./Buttons.jsx";
 import buttonStyles from "./Button.module.css";
 
 function DefaultButton() {
-  // const buttonStyles = {
-  //   width: "fitContent",
-  //   padding: "0.5rem 1rem",
-  //   borderRadius: "0.5rem",
-  //   backgroundColor: "rgba(15, 23, 42, 1)",
-  //   color: "rgba(255, 255, 255, 1)",
-  //   outline: "transparent",
-  //   border: "transparent",
-  //   cursor: "pointer",
-  // };
-
   const [isDialog, setIsDialog] = useState(false);
 
   const openDialog = () => setIsDialog(true);
