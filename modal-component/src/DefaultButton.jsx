a2406a92b4a7c8552664d8fb31727e1024a0cdef
Eniola Sola-Kehinde <114470813+EverPleroma@users.noreply.github.com>
2024-10-31 21:51:52 +0100

    Alert dialog modal created

---
+++ modal-component/src/DefaultButton.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import AlertDialog from "./AlertDialog.jsx";
+
+function DefaultButton() {
+  const buttonStyles = {
+    width: "fitContent",
+    padding: "0.5rem 1rem",
+    borderRadius: "0.5rem",
+    backgroundColor: "rgba(15, 23, 42, 1)",
+    color: "rgba(255, 255, 255, 1)",
+    outline: "transparent",
+    border: "transparent",
+    cursor: "pointer",
+  };
+
+  const [isDialog, setIsDialog] = useState(false);
+
+  const openDialog = () => {
+    setIsDialog(true);
+  };
+
+  const closeDialog = () => {
+    setIsDialog(false);
+  };
+
+  return (
+    <>
+      <div style={{ marginTop: "30%" }}>
+        <button style={buttonStyles} onClick={openDialog}>
+          Delete
+        </button>
+      </div>
+      {isDialog ? <AlertDialog handleClick={closeDialog} /> : null}
+    </>
+  );
+}
+
+export default DefaultButton;
